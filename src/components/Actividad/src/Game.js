bedd204115de858cd4446846770b2d653d0df43e
cristian <crisdamencast@gmail.com>
2021-10-22 19:33:15 -0500

    canvas

--- src/components/Actividad/src/Game.js
+++ src/components/Actividad/src/Game.js
@@ -6,11 +6,16 @@ class Game {
 
     }
 
+    setup(app) {
+        app.createCanvas(1280, 720);
+    }
+
     draw(app) {
+        app.background(0);
         app.text("ESTE ES EL JUEGO", 100, 100)
         app.rect(100, 200, 100, 100)
         app.text("xdddd", 100, 100)
-        //Hola hijueputa
+            //Hola hijueputa
     }
 
     mousePressed() {
